199532d65ddb8df503971bc792a2ce0f69f02f6f
Martin Buhr <martin@tyk.io>
2016-08-22 09:34:26 -0400

    First commit

---
+++ goverify_test.go
@@ -0,0 +1,64 @@
+package goverify
+
+import (
+	"encoding/base64"
+	"fmt"
+	"testing"
+)
+
+var testPublicKey string = `-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDCFENGw33yGihy92pDjZQhl0C3
+6rPJj+CvfSC8+q28hxA161QFNUd13wuCTUcq0Qd2qsBe/2hFyc2DCJJg0h1L78+6
+Z4UMR7EOcpfdUE9Hf3m/hs+FUR45uBJeDK1HSFHD8bHKD6kv8FPGfJTotc+2xjJw
+oYi+1hqp1fIekaxsyQIDAQAB
+-----END PUBLIC KEY-----`
+
+var testPrivateKey string = `-----BEGIN RSA PRIVATE KEY-----
+MIICXgIBAAKBgQDCFENGw33yGihy92pDjZQhl0C36rPJj+CvfSC8+q28hxA161QF
+NUd13wuCTUcq0Qd2qsBe/2hFyc2DCJJg0h1L78+6Z4UMR7EOcpfdUE9Hf3m/hs+F
+UR45uBJeDK1HSFHD8bHKD6kv8FPGfJTotc+2xjJwoYi+1hqp1fIekaxsyQIDAQAB
+AoGBAJR8ZkCUvx5kzv+utdl7T5MnordT1TvoXXJGXK7ZZ+UuvMNUCdN2QPc4sBiA
+QWvLw1cSKt5DsKZ8UETpYPy8pPYnnDEz2dDYiaew9+xEpubyeW2oH4Zx71wqBtOK
+kqwrXa/pzdpiucRRjk6vE6YY7EBBs/g7uanVpGibOVAEsqH1AkEA7DkjVH28WDUg
+f1nqvfn2Kj6CT7nIcE3jGJsZZ7zlZmBmHFDONMLUrXR/Zm3pR5m0tCmBqa5RK95u
+412jt1dPIwJBANJT3v8pnkth48bQo/fKel6uEYyboRtA5/uHuHkZ6FQF7OUkGogc
+mSJluOdc5t6hI1VsLn0QZEjQZMEOWr+wKSMCQQCC4kXJEsHAve77oP6HtG/IiEn7
+kpyUXRNvFsDE0czpJJBvL/aRFUJxuRK91jhjC68sA7NsKMGg5OXb5I5Jj36xAkEA
+gIT7aFOYBFwGgQAQkWNKLvySgKbAZRTeLBacpHMuQdl1DfdntvAyqpAZ0lY0RKmW
+G6aFKaqQfOXKCyWoUiVknQJAXrlgySFci/2ueKlIE1QqIiLSZ8V8OlpFLRnb1pzI
+7U1yQXnTAEFYM560yJlzUpOb1V4cScGd365tiSMvxLOvTA==
+-----END RSA PRIVATE KEY-----`
+
+func TestSignAndVerify(t *testing.T) {
+	signer, err := LoadPrivateKeyFromString(testPrivateKey)
+	if err != nil {
+		t.Errorf("signer is damaged: %v", err)
+	}
+
+	toSign := "date: Thu, 05 Jan 2012 21:31:40 GMT"
+
+	signed, err := signer.Sign([]byte(toSign))
+	if err != nil {
+		fmt.Errorf("could not sign request: %v", err)
+	}
+	sig := base64.StdEncoding.EncodeToString(signed)
+	fmt.Printf("Signature: %v\n", sig)
+
+	var signtureExpected string = "ATp0r26dbMIxOopqw0OfABDT7CKMIoENumuruOtarj8n/97Q3htHFYpH8yOSQk3Z5zh8UxUym6FYTb5+A0Nz3NRsXJibnYi7brE/4tx5But9kkFGzG+xpUmimN4c3TMN7OFH//+r8hBf7BT9/GmHDUVZT2JzWGLZES2xDOUuMtA="
+	if sig != signtureExpected {
+		t.Error("Signatures do not match! Got: ")
+		t.Error(sig)
+	}
+
+	parser, perr := LoadPublicKeyFromString(testPublicKey)
+	if perr != nil {
+		t.Errorf("could not sign request: %v", err)
+	}
+
+	err = parser.Verify([]byte(toSign), signed)
+
+	if err != nil {
+		t.Error("Validation should have succeeded, got error: ", err)
+	}
+
+}
